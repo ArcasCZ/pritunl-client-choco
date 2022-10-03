e1285b8489182b9c7b40110b061d8f61dc632414
ArcasCZ <VydraMan@seznam.cz>
2022-10-03 11:31:07 +0200

    Update Pritunl Client to 1.3.3000.95

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= $env:ChocolateyPackageName
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/pritunl/pritunl-client-electron/releases/download/1.3.3289.33/Pritunl.exe'
+$url        = 'https://github.com/pritunl/pritunl-client-electron/releases/download/1.3.3300.95/Pritunl.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'pritunl client*'
-  checksum      = '543cc836ca0babb2335a3f2a6e9dd12feeedd2e0fdca3b6114a275037555c58e'
+  checksum      = 'acc83bb7e90694f549247d6dde06b70db30d7b3e1f8bf9eef4f0c70b990a4317'
   checksumType  = 'sha256'
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0,1)
