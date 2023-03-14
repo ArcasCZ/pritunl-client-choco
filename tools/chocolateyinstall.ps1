e65394baacfb759e9fbc0c90cd8b4f8812c08e95
ArcasCZ <VydraMan@seznam.cz>
2023-03-14 12:16:57 +0100

    Bump version to 1.3.3467.51

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -2,7 +2,7 @@
 
 $packageName= $env:ChocolateyPackageName
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/pritunl/pritunl-client-electron/releases/download/1.3.3464.45/Pritunl.exe'
+$url        = 'https://github.com/pritunl/pritunl-client-electron/releases/download/1.3.3467.51/Pritunl.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -10,7 +10,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'pritunl client*'
-  checksum      = 'ef221433e88c2c93f75ed6b0456423d7bed9a86ed1a33aff2c0e997b2c63eab3'
+  checksum      = 'cccba38e85a9f5718fc20a9ac94ecdbb180ee9ae5a488fbbbb63fddcc9a2ba00'
   checksumType  = 'sha256'
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0,1)
