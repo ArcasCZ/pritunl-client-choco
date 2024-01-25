25746aa5c0d1fd8adda53f6f2d4069df5b2ff0c3
ArcasCZ <VydraMan@seznam.cz>
2024-01-25 18:07:23 +0100

    Update Pritunl to newest version

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -4,7 +4,7 @@ $ErrorActionPreference = 'Stop';
 
 $packageName= $env:ChocolateyPackageName
 $toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://github.com/pritunl/pritunl-client-electron/releases/download/1.3.3772.10/Pritunl.exe'
+$url        = 'https://github.com/pritunl/pritunl-client-electron/releases/download/1.3.3785.81/Pritunl.exe'
 
 $packageArgs = @{
   packageName   = $packageName
@@ -12,7 +12,7 @@ $packageArgs = @{
   fileType      = 'EXE'
   url           = $url
   softwareName  = 'pritunl client*'
-  checksum      = 'beaedde9249ad3c0e6eee8cc04a277c03e69f29ce32cbe598c39de0d9275c3f6'
+  checksum      = 'd5602a199d2f1bb3dc43f8eb94c2b02175f4668eb8beb7e6860713024025cb33'
   checksumType  = 'sha256'
   silentArgs   = '/VERYSILENT /SUPPRESSMSGBOXES /NORESTART /SP-'
   validExitCodes= @(0,1)
